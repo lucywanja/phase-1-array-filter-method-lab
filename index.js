ea561da70dc1539521efb0d76f74511fa5951ff8
Lucy Kariuki <kariuki.lucy@strathmore.edu>
2024-04-04 09:46:26 +0300

    completed the lab with filter method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === name.toLowerCase()
+    );
+  }
+ 
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
